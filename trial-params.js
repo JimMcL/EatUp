6e223d06b73bb6eb9cbb79dfaa0580607e35ac39
Jim McLean <jim_mclean@optusnet.com.au>
2019-08-11 09:52:56 +0100

    Fix bugs.

---
+++ trial-params.js
@@ -0,0 +1,8 @@
+// Photo timeout (i.e. time until "escape") in milliseconds
+var escapeTimeout = 10000;
+
+// Number of photos to show in a single trial
+var numPhotos = 12;
+
+// Show user their mistakes?
+var showUserMistakes = true;
