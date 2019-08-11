70d1863bc9349b60b270482ce3ba1dc41396085d
Jim McLean <jim_mclean@optusnet.com.au>
2019-08-11 08:01:23 +0100

    Start to add in performance assessment to make it more fun.

---
+++ setup.js
@@ -0,0 +1,72 @@
+"use strict";
+// Some functions which are probably specific to my application
+
+// Returns true if we are in debug mode. Debug mode is invoked if: (1)
+// the document is a local file (i.e. protocol == "file:", or (2) the
+// URL has a query parameter "debug=T"
+function IsDebugging() {
+    return GetUrlParam("debug") == "T" || window.location.protocol == "file:";
+}
+
+// Chooses (and returns) a logger. Normally it will be a Firebase
+// logger, but when debugging, it will be a console logger.  
+//
+// If running in debug mode, the "#watermark" style.display is cleared
+function ChooseLogger(debug) {
+    // Get the data logger
+    var logger = null;
+    if (debug) {
+        // This logger just writes to the console
+        logger = new ConsoleDataLogger(GetUserId());
+        // Display debug symbol
+        var ele = document.getElementById("watermark");
+        if (ele)
+            ele.style.display = "";
+    } else {
+        // This logger writes to a Firebase database
+        InitFirebase();
+        logger = new FirebaseLogger(GetUserId());
+    }
+    return logger;
+}
+
+
+// Downloads the photo list and starts the trial.
+// Assumes that papaparse is loaded.
+function PrepareAndStartTrial(logger, photosPerTrial, photocsvUrl, photoEleId, escapeTimeout, animationDuration, shortcutKeys) {
+
+    // Invoked once we have the list of photos
+    function prepare(data) {
+        // Derive columns required by the PhotoSeq constructor
+        var candidatePhotos = data.map(n => {
+            n.correctScore = n.family = "Formicidae" ? "ant" : "notAnt";
+            n.url = n.webUrl;
+            // Show more ants to get an even apparent mix of ants/non-ants
+            n.weight = n.correctScore == "ant" ? 1.5 : 1;
+            n.known = n.Type;
+            return n;
+        });
+
+        // Prepare the photo list from the candidate photos
+        var photos = new PhotoSeq(candidatePhotos, photosPerTrial);
+
+        // Start the trial
+        new Trial(logger, photos, photoEleId, escapeTimeout, animationDuration).prepare(shortcutKeys);
+    }
+
+    var url = "photo_info.csv";
+    url = "http://jimmcl.github.io/EatUp/photo_info.csv"; // TODO delete this line
+    Papa.parse(url, {
+	download: true,
+        header: true,
+        skipEmptyLines: true,
+	complete: function(results) {
+            if (results.errors.length > 0) {
+                alert("Error retrieving photos to be displayed: " + results.errors[0].message + ", row " + results.errors[0].row);
+            } else {
+                // Construct photo list and start the trial
+                prepare(results.data);
+            }
+	}
+    });
+}
